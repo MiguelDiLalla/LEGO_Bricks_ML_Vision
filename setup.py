b0f40c7526aa3b89b0cfefdcb9fc0277ab38bd2e
Miguel Di Lalla <mdll71421@gmail.com>
2025-01-31 14:48:20 +0000

    Update version to 2.0.0 and change author and repository URL in setup files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lego-vision-cli",
-    version="1.0.0",
+    version="2.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -21,11 +21,11 @@ setup(
             "lego-export-results=scripts.cli:run_export_results"
         ]
     },
-    author="Your Name",
+    author="Miguel Di Lalla",
     description="CLI for managing LEGO Bricks ML pipeline",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/your-repo",
+    url="https://github.com/MiguelDiLalla/LEGO_Bricks_ML",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
